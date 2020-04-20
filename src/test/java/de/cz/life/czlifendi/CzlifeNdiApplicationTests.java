40245435ab37c24315904b636ca2daa9645e2789
gituserDoesNotExist <manfred-beutel@web.de>
2020-04-20 22:52:29 +0200

    init project

---
+++ src/test/java/de/cz/life/czlifendi/CzlifeNdiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cz.life.czlifendi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzlifeNdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
